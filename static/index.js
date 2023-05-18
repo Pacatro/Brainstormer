ea5fec6dbbb6d5166c85e19028735d0dbaba0051
Pacatro <paco.algar.15@gmail.com>
2023-05-19 00:47:58 +0200

    Brainstormer

---
+++ static/index.js
@@ -0,0 +1,9 @@
+function buttonText() {
+    const button = document.getElementById("generate-button")
+    const phrases = ["New Idea!", "Eureka!", "Lets start!"]
+
+    button.innerHTML = phrases[Math.floor((Math.random()*phrases.length))]
+    console.log(button.value)
+}
+
+buttonText()
