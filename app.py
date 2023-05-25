d8935ca4235e8d17227ed0a792b266a1cf71bdf2
Pacatro <paco.algar.15@gmail.com>
2023-05-25 22:31:29 +0200

    finish design login-signup forms

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -16,7 +16,8 @@ def login():
 
 @app.route('/create_account')
 def create_account():
-    return render_template('create_account.html')
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
